c397a218a69ed21f4021405559fcbed7a6ea03ac
fin2018 <35458854+fin2018@users.noreply.github.com>
2018-01-17 14:10:02 +0700

    Create newdeb.sh

---
+++ newdeb.sh
@@ -0,0 +1,313 @@
+#!/bin/sh
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0'`;
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+
+# go to root
+cd
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+# install wget and curl
+apt-get update;apt-get -y install wget curl;
+
+# set time GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+service ssh restart
+
+# set repo 
+wget -O /etc/apt/sources.list "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/sources.list.debian7" 
+wget "http://www.dotdeb.org/dotdeb.gpg" 
+wget "http://www.webmin.com/jcameron-key.asc" 
+cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg 
+cat jcameron-key.asc | apt-key add -;rm jcameron-key.asc 
+
+# remove unused
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove sendmail*;
+apt-get -y --purge remove bind9*;
+
+# update
+apt-get update; apt-get -y upgrade;
+
+# install webserver
+apt-get -y install nginx php5-fpm php5-cli
+
+# install essential package
+echo "mrtg mrtg/conf_mods boolean true" | debconf-set-selections
+apt-get -y install bmon iftop htop nmap axel nano iptables traceroute sysv-rc-conf dnsutils bc nethogs openvpn vnstat less screen psmisc apt-file whois ptunnel ngrep mtr git zsh mrtg snmp snmpd snmp-mibs-downloader unzip unrar rsyslog debsums rkhunter
+apt-get -y install build-essential
+
+# disable exim
+service exim4 stop
+sysv-rc-conf exim4 off
+
+# update apt-file
+apt-file update
+
+# setting vnstat
+vnstat -u -i venet0
+service vnstat restart
+
+# install neofetch
+echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | sudo tee -a /etc/apt/sources.list
+curl -L "https://bintray.com/user/downloadSubjectPublicKey?username=bintray" -o Release-neofetch.key && sudo apt-key add Release-neofetch.key && rm Release-neofetch.key
+apt-get update
+apt-get install neofetch
+
+# install screenfetch
+cd
+wget 'https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/screenfetch-dev'
+mv screenfetch-dev /usr/bin/screenfetch-dev
+chmod +x /usr/bin/screenfetch-dev
+echo "clear" >> .profile
+echo "screenfetch-dev" >> .profile
+
+# install webserver
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.github.com/paanfinest/debian7_32bit/master/nginx.conf"
+mkdir -p /home/vps/public_html
+echo "<pre>Modified by Pa'an Finest</pre>" > /home/vps/public_html/index.html
+echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.github.com/paanfinest/debian7_32bit/master/vps.conf"
+sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
+service php5-fpm restart
+service nginx restart 
+
+# install openvpn
+wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/yurisshOS/debian7/master/openvpn-debian.tar"
+cd /etc/openvpn/
+tar xf openvpn.tar
+wget -O /etc/openvpn/1194.conf "https://raw.github.com/yurisshOS/debian7/master/1194.conf"
+service openvpn restart
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+wget -O /etc/iptables.up.rules "https://raw.github.com/yurisshOS/debian7/master/iptables.up.rules"
+sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
+sed -i $MYIP2 /etc/iptables.up.rules;
+iptables-restore < /etc/iptables.up.rules
+service openvpn restart
+#wget https://raw.github.com/cwaffles/ezopenvpn/master/ezopenvpn.sh --no-check-certificate -O ezopenvpn.sh
+#chmod +x ezopenvpn.sh
+#./ezopenvpn.sh
+
+# configure openvpn client config
+cd /etc/openvpn/
+wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/paanfinest/debian7_32bit/master/1194-client.conf"
+sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
+PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
+useradd -M -s /bin/false YurisshOS
+echo "Paanfinest:$PASS" | chpasswd
+echo "username" >> pass.txt
+echo "password" >> pass.txt
+tar cf client.tar 1194-client.ovpn pass.txt
+cp client.tar /home/vps/public_html/
+cd
+
+# install badvpn 
+wget -O /usr/bin/badvpn-udpgw "https://github.com/ForNesiaFreak/FNS/raw/master/sett/badvpn-udpgw" 
+if [ "$OS" == "x86_64" ]; then 
+  wget -O /usr/bin/badvpn-udpgw "https://github.com/ForNesiaFreak/FNS/raw/master/sett/badvpn-udpgw64" 
+fi 
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local 
+chmod +x /usr/bin/badvpn-udpgw 
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
+
+# install mrtg
+wget -O /etc/snmp/snmpd.conf "https://raw.github.com/yurisshOS/debian7/master/snmpd.conf"
+wget -O /root/mrtg-mem.sh "https://raw.github.com/fin2018/VPS/master/mrtg-mem.sh"
+chmod +x /root/mrtg-mem.sh
+cd /etc/snmp/
+sed -i 's/TRAPDRUN=no/TRAPDRUN=yes/g' /etc/default/snmpd
+service snmpd restart
+snmpwalk -v 1 -c public localhost 1.3.6.1.4.1.2021.10.1.3.1
+mkdir -p /home/vps/public_html/mrtg
+cfgmaker --zero-speed 100000000 --global 'WorkDir: /home/vps/public_html/mrtg' --output /etc/mrtg.cfg public@localhost
+curl "https://raw.github.com/yurisshOS/debian7/master/mrtg.conf" >> /etc/mrtg.cfg
+sed -i 's/WorkDir: \/var\/www\/mrtg/# WorkDir: \/var\/www\/mrtg/g' /etc/mrtg.cfg
+sed -i 's/# Options\[_\]: growright, bits/Options\[_\]: growright/g' /etc/mrtg.cfg
+indexmaker --output=/home/vps/public_html/mrtg/index.html /etc/mrtg.cfg
+if [ -x /usr/bin/mrtg ] && [ -r /etc/mrtg.cfg ]; then mkdir -p /var/log/mrtg ; env LANG=C /usr/bin/mrtg /etc/mrtg.cfg 2>&1 | tee -a /var/log/mrtg/mrtg.log ; fi
+if [ -x /usr/bin/mrtg ] && [ -r /etc/mrtg.cfg ]; then mkdir -p /var/log/mrtg ; env LANG=C /usr/bin/mrtg /etc/mrtg.cfg 2>&1 | tee -a /var/log/mrtg/mrtg.log ; fi
+if [ -x /usr/bin/mrtg ] && [ -r /etc/mrtg.cfg ]; then mkdir -p /var/log/mrtg ; env LANG=C /usr/bin/mrtg /etc/mrtg.cfg 2>&1 | tee -a /var/log/mrtg/mrtg.log ; fi
+cd
+
+# setting port ssh
+sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port  80' /etc/ssh/sshd_config
+sed -i 's/Port 22/Port  22/g' /etc/ssh/sshd_config
+service ssh restart
+
+#Blockir Torrent
+iptables -A OUTPUT -p tcp --dport 6881:6889 -j DROP
+iptables -A OUTPUT -p udp --dport 1024:65534 -j DROP
+iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
+iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
+
+# install dropbear
+apt-get -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109 -p 110"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+service ssh restart
+service dropbear restart
+
+# install stunnel 
+apt-get install stunnel4 -y
+wget -O /etc/stunnel/stunnel.conf "https://gist.githubusercontent.com/bwann/82ed679e94972666808d97587d276677/raw/d26838667973a94a403a0c1bade6793dd83e9e5d/stunnel.conf"
+wget -O /etc/stunnel/stunnel.pem "https://raw.githubusercontent.com/curl/curl/master/tests/stunnel.pem"
+wget -O /etc/stunnel/stunnel-client.conf "https://gist.githubusercontent.com/bwann/82ed679e94972666808d97587d276677/raw/d26838667973a94a403a0c1bade6793dd83e9e5d/stunnel-client.conf"
+openssl genrsa -out key.pem 2048
+openssl req -new -x509 -days 365 -nodes -out stunnel.pem -keyout stunnel.pem
+cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+service stunnel4 restart
+
+# install vnstat gui
+cd /home/vps/public_html/
+wget http://www.sqweek.com/sqweek/files/vnstat_php_frontend-1.5.1.tar.gz
+tar xf vnstat_php_frontend-1.5.1.tar.gz
+rm vnstat_php_frontend-1.5.1.tar.gz
+mv vnstat_php_frontend-1.5.1 vnstat
+cd vnstat
+sed -i 's/eth0/venet0/g' config.php
+sed -i "s/\$iface_list = array('venet0', 'sixxs');/\$iface_list = array('venet0');/g" config.php
+sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+sed -i 's/Internal/Internet/g' config.php
+sed -i '/SixXS IPv6/d' config.php
+cd
+
+# install fail2ban
+apt-get -y install fail2ban;service fail2ban restart
+
+# Instal DDOS Flate
+if [ -d '/usr/local/ddos' ]; then
+	echo; echo; echo "Please un-install the previous version first"
+	exit 0
+else
+	mkdir /usr/local/ddos
+fi
+clear
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+echo -n '.'
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+echo -n '.'
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+echo -n '.'
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
+echo '...done'
+echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+
+# install squid3
+apt-get -y install squid3
+wget -O /etc/squid3/squid.conf "https://raw.github.com/fin2018/VPS/master/squid3.conf"
+sed -i $MYIP2 /etc/squid3/squid.conf;
+service squid3 restart
+
+# install webmin
+cd
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.680_all.deb
+dpkg -i --force-all webmin_1.680_all.deb;
+apt-get -y -f install;
+rm /root/webmin_1.680_all.deb
+service webmin restart
+service vnstat restart
+
+# info
+clear
+echo "Autoscript Include:" | tee log-install.txt
+echo "=======================================================" | tee -a log-install.txt
+echo "Service :" | tee -a log-install.txt
+echo "---------" | tee -a log-install.txt
+echo "OpenSSH  : 22, 143" | tee -a log-install.txt
+echo "Dropbear : 443, 110, 109" | tee -a log-install.txt
+echo "Squid3   : 80, 8000, 8080, 3128 (limit to IP $MYIP)" | tee -a log-install.txt
+#echo "OpenVPN  : TCP 1194 (client config : http://$MYIP:81/client.ovpn)" | tee -a log-install.txt
+echo "badvpn   : badvpn-udpgw port 7300" | tee -a log-install.txt
+echo "PPTP VPN : TCP 1723" | tee -a log-install.txt
+echo "nginx    : 81" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "Tools :" | tee -a log-install.txt
+echo "-------" | tee -a log-install.txt
+echo "axel, bmon, htop, iftop, mtr, rkhunter, nethogs: nethogs $ether" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "Script :" | tee -a log-install.txt
+echo "--------" | tee -a log-install.txt
+echo "screenfetch" | tee -a log-install.txt
+echo "menu (Menu Script VPS via Putty) :" | tee -a log-install.txt
+echo "  - Buat Akun SSH/OpenVPN (user-add)" | tee -a log-install.txt
+echo "  - Generate Akun SSH/OpenVPN (user-gen)" | tee -a log-install.txt
+echo "  - Ganti Password Akun SSH/OpenVPN (user-pass)" | tee -a log-install.txt
+echo "  - Tambah Masa Aktif Akun SSH/OpenVPN (user-renew)" | tee -a log-install.txt
+echo "  - Hapus Akun SSH/OpenVPN (user-del)" | tee -a log-install.txt
+echo "  - Buat Akun PPTP VPN (user-add-pptp)" | tee -a log-install.txt
+echo "  - Monitoring Dropbear (dropmon [PORT])" | tee -a log-install.txt
+echo "  - Cek Login Dropbear, OpenSSH, PPTP VPN dan OpenVPN (user-login)" | tee -a log-install.txt
+echo "  - Kill Multi Login Manual (1-2 Login) (user-limit [x])" | tee -a log-install.txt
+echo "  - Daftar Akun dan Expire Date (user-list)" | tee -a log-install.txt
+echo "  - Daftar Akun Aktif (user-active-list)" | tee -a log-install.txt
+echo "  - Daftar Akun Expire (user-expire-list)" | tee -a log-install.txt
+echo "  - Disable Akun Expire (user-expire)" | tee -a log-install.txt
+echo "  - Memory Usage (ps-mem)" | tee -a log-install.txt
+echo "  - Speedtest (speedtest --share)" | tee -a log-install.txt
+echo "  - Benchmark (benchmark)" | tee -a log-install.txt
+echo "  - Reboot Server (reboot)" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "Fitur lain :" | tee -a log-install.txt
+echo "------------" | tee -a log-install.txt
+echo "Webmin         : http://$MYIP:10000/" | tee -a log-install.txt
+echo "vnstat         : http://$MYIP:81/vnstat/ (Cek Bandwith)" | tee -a log-install.txt
+echo "MRTG           : http://$MYIP:81/mrtg/" | tee -a log-install.txt
+echo "Timezone       : Asia/Jakarta (GMT +7)" | tee -a log-install.txt
+echo "Fail2Ban       : [on]" | tee -a log-install.txt
+echo "(D)DoS Deflate : [on]" | tee -a log-install.txt
+echo "Block Torrent  : [off]" | tee -a log-install.txt
+echo "IPv6           : [off]" | tee -a log-install.txt
+#echo "Autolimit 2 bitvise per IP to all port (port 22, 143, 109, 110, 443, 1194, 7300 TCP/UDP)" | tee -a log-install.txt
+echo "Auto Lock User Expire tiap jam 00:00" | tee -a log-install.txt
+echo "Auto Reboot tiap jam 00:00 dan jam 12:00" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "Script By Pa'an Finest" | tee -a log-install.txt
+echo "Support By Finest Media - Phreakers Jateng Official" | tee -a log-install.txt
+echo "Thanks to Original Creator Kang Arie & Mikodemos" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "Log Instalasi --> /root/log-install.txt" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "SILAHKAN REBOOT VPS ANDA !" | tee -a log-install.txt
+echo "=======================================================" | tee -a log-install.txt
+cd 
+rm -f /root/newdeb.sh
+rm -f /root/pptp.sh
+rm -f /root/dropbear-2012.55.tar.bz2
+rm -rf /root/dropbear-2012.55
